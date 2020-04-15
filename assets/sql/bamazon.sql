8321a56a5cfe2e9fa3ee0ff17e6410b48bce1fba
alvaroluna <luna.alvaro3@gmail.com>
2020-04-14 18:53:59 -0700

    Began inserting values in sql

--- assets/sql/bamazon.sql
+++ assets/sql/bamazon.sql
@@ -1,8 +1,12 @@
+-- not sure what this does anymore but keeping this here for reference
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Cinnamon1' 
+flush privileges;
+
 DROP DATABASE IF EXISTS bamazon;
+
 CREATE DATABASE bamazon;
 
 USE bamazon;
-
 CREATE TABLE products(
   item_id INT NOT NULL AUTO_INCREMENT,
   product_name VARCHAR(30),
@@ -11,3 +15,5 @@ CREATE TABLE products(
   stock_quantity INT(6) DEFAULT 2000,
   PRIMARY KEY (item_id)
   );
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)VALUES ("WidgetA", "Electronics", 100.00, 200);
